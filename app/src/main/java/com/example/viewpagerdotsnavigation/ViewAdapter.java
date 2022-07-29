6c0be452ffb1a956d1dd77785e04364743e3987a
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-07-29 16:05:51 +0500

    initial commit

---
+++ app/src/main/java/com/example/viewpagerdotsnavigation/ViewAdapter.java
@@ -0,0 +1,54 @@
+package com.example.viewpagerdotsnavigation;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class ViewAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images={R.drawable.ic_1,R.drawable.ic_2,R.drawable.ic_3,R.drawable.ic_4,R.drawable.ic_5};
+
+    public ViewAdapter(Context context)
+    {
+        this.context=context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater=(LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE
+        );
+        View view =layoutInflater.inflate(R.layout.item,null);
+        ImageView imageView=view.findViewById(R.id.image_view);
+        imageView.setImageResource(images[position]);
+        ViewPager viewPager=(ViewPager) container;
+        viewPager.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager=(ViewPager) container;
+        View view=(View) object;
+        viewPager.removeView(view);
+    }
+}
